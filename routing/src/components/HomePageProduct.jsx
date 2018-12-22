f9674a73cb42f294611c3f91b5cbfd38b1cc19c6
Salome <sbabu15@freeuni.edu.ge>
2018-12-22 14:56:55 +0400

    change home page

--- routing/src/components/HomePageProduct.jsx
+++ routing/src/components/HomePageProduct.jsx
@@ -28,10 +28,8 @@
 //                 </div>
 //             </div>
 //           )
-//         } 
+//         }
 //        </div>
-
-
 
 //         </div>
 //         </div>
@@ -41,15 +39,7 @@
 
 // }
 
-
-
-
 // export default HomePageProduct;
-
-
-
-
-
 
 import React, { Component } from "react";
 import data from "../db/products.json";
@@ -61,10 +51,12 @@ class HomePageProduct extends Component {
     this.state = {
       data
     };
+
+    this.availableGrids = [];
   }
 
   componentDidMount() {
-    this.dataLength()
+    this.dataLength();
   }
 
   // Sort products by id descending
@@ -74,47 +66,76 @@ class HomePageProduct extends Component {
 
   // get current array length
 
-
   // if an array has more then 9 element trim it    /ანუ  აქ  მინდა რომ  json ფაილიდან  წამოიღოს  ბოლო  9 პროდუქტის მონაცემი
   dataLength = () => {
     let dataLength = this.state.data.length;
     if (dataLength > 9) {
-      let lastNine = this.state.data.slice(dataLength - 9)
+      let lastNine = this.state.data.slice(dataLength - 9);
       this.setState({
         data: lastNine
-      })
+      });
+
+      lastNine.map((val, key) => {
+        if (key % 3 == 0) {
+          this.availableGrids.push(key);
+        }
+      });
+
+      console.log("grid:", this.availableGrids);
     }
-  }
+  };
 
+  retreiveItems = startKey => {
+    let itemsToReturn = [];
+    for (let i = startKey; i < startKey + 3; i++) {
+      itemsToReturn.push(this.state.data[i]);
+    }
+    return itemsToReturn;
+  };
 
-
-render() {
-  return (
-
-
-        <div className="right-side">
-
-          <div className="gallery-container">
-          
-              {this.state.data.map((value, index) => (
-                <div key={index} className="single-item">
-                <div className="gray-bg">
-                  <div className="ProductImage">
-                    <img src={value.url} alt="" />
+  render() {
+    return (
+      <div className="right-side">
+        <div className="gallery-container">
+          {this.availableGrids.map((value, iKey) => (
+            <div key={iKey}  className={"a"+iKey} >       
+        
+              {this.retreiveItems(value).map((item, iItem) => (
+               
+                <div key={iItem} className="single-item">
+                 <div className="gray-bg">
+             
+                    <div className="ProductImage">
+                      <img src={item.url} alt="" />
+                    </div>
+                    <div className="ProductInfo">
+                      <p>{item.name}</p>
+                      <h2>price: {item.price}</h2>
+                    </div>
                   </div>
-                  <div className="ProductInfo">
-                    <p>{value.name}</p>
-                    <h2>price: {value.price}</h2>
-                  </div>
-                  </div>
-                </div>
+               </div>
               ))}
-              
+            </div>
+          ))}
+        </div>
+        {/* <div className="gallery-container">
+          {this.state.data.map((value, index) => (
+            <div key={index} className="single-item">
+              <div className="gray-bg">
+                <div className="ProductImage">
+                  <img src={value.url} alt="" />
+                </div>
+                <div className="ProductInfo">
+                  <p>{value.name}</p>
+                  <h2>price: {value.price}</h2>
+                </div>
               </div>
-              </div>
-  );
-
-}
+            </div>
+          ))}
+        </div> */}
+      </div>
+    );
+  }
 }
 
 export default HomePageProduct;
