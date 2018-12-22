02f8900f197cc53880945809f61efa100b1e1b17
Salome <sbabu15@freeuni.edu.ge>
2018-12-22 23:09:41 +0400

    last changes about product  display

--- routing/src/components/Footer.jsx
+++ routing/src/components/Footer.jsx
@@ -40,4 +40,6 @@ class Footer extends Component {
   }
 }
 
+
+
 export default Footer;
--- routing/src/components/HomePageProduct.jsx
+++ routing/src/components/HomePageProduct.jsx
@@ -1,46 +1,3 @@
-// import React, { Component } from "react";
-// import data from "../db/products.json";
-
-// class HomePageProduct extends Component {
-//   constructor(props) {
-//       super(props)
-
-//       this.state={
-//         data
-//       }
-//   }
-
-//   render() {
-//     return (
-//       <div className="right-side">
-//       <div className="gallery-container">
-//       <div className="first-grid">
-
-//         {
-//           this.state.data.map((value) =>
-//           <div className="single-item">
-//                 <div className="ProductImage">
-//                   <img src={value.url} alt="" />
-//                 </div>
-//                 <div className="ProductInfo">
-//                   <p>{value.name}</p>
-//                   <h2>price: {value.price}</h2>
-//                 </div>
-//             </div>
-//           )
-//         }
-//        </div>
-
-//         </div>
-//         </div>
-
-//     );
-//   }
-
-// }
-
-// export default HomePageProduct;
-
 import React, { Component } from "react";
 import data from "../db/products.json";
 
@@ -59,14 +16,6 @@ class HomePageProduct extends Component {
     this.dataLength();
   }
 
-  // Sort products by id descending
-  // this.state.data.sort((a, b) => {
-  //   return b.id - a.id;
-  // });
-
-  // get current array length
-
-  // if an array has more then 9 element trim it    /ანუ  აქ  მინდა რომ  json ფაილიდან  წამოიღოს  ბოლო  9 პროდუქტის მონაცემი
   dataLength = () => {
     let dataLength = this.state.data.length;
     if (dataLength > 9) {
@@ -98,41 +47,23 @@ class HomePageProduct extends Component {
       <div className="right-side">
         <div className="gallery-container">
           {this.availableGrids.map((value, iKey) => (
-            <div key={iKey}  className={"a"+iKey} >       
-        
+            <div key={iKey} className={"a" + iKey}>
               {this.retreiveItems(value).map((item, iItem) => (
-               
                 <div key={iItem} className="single-item">
-                 <div className="gray-bg">
-             
+                  <div className="gray-bg">
                     <div className="ProductImage">
                       <img src={item.url} alt="" />
                     </div>
                     <div className="ProductInfo">
-                      <p>{item.name}</p>
-                      <h2>price: {item.price}</h2>
+                      <p>price: {item.price}</p>
+                      <h3>{item.name}</h3>
                     </div>
                   </div>
-               </div>
+                </div>
               ))}
             </div>
           ))}
         </div>
-        {/* <div className="gallery-container">
-          {this.state.data.map((value, index) => (
-            <div key={index} className="single-item">
-              <div className="gray-bg">
-                <div className="ProductImage">
-                  <img src={value.url} alt="" />
-                </div>
-                <div className="ProductInfo">
-                  <p>{value.name}</p>
-                  <h2>price: {value.price}</h2>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div> */}
       </div>
     );
   }
