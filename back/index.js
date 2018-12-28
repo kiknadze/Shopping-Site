4d18d1163e7f4aba5be21ddb76e2680327c44a63
TEMO <temo.kiknadze@yahoo.com>
2018-12-28 21:10:02 +0400

    Upload site to Heroku

--- back/index.js
+++ back/index.js
@@ -34,6 +34,14 @@ const encrypt = data => {
   return hash;
 };
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("routing/build"));
+ 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "routing", "build", "index.html"));
+  });
+ }
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
